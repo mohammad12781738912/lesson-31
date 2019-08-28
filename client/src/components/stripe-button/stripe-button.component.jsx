a9e9840bbb82ef139371f16185131e97a79ce96f
mohammad12781738912 <53177534+mohammad12781738912@users.noreply.github.com>
2019-08-28 20:46:41 +0300

    Update stripe-button.component.jsx

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
-  const publishableKey = 'pk_test_b7a3hFL5nC3qlBCZ6bQACpez00gyMMP52H';
+  const publishableKey = 'pk_test_46f02RvNvw7rTrCdQ2kwLA7n00Ixb1X6Q2';
 
   const onToken = token => {
     axios({
